d6537a814ee69bafd5391e271392117680bc9fb4
Travis <travis@localhost.dev>
2018-08-06 22:53:46 +1000

    Add/update tests

--- animal_test.go
+++ animal_test.go
@@ -8,7 +8,7 @@ func TestAnimal(t *testing.T) {
 	c := NewChance()
 	actual := c.Animal()
 	if len(actual) <= 0 {
-		t.Errorf("Animal() was incorrect, expect: [1 or more character], actual: %s.", actual)
+		t.Errorf("Animal() was incorrect, expect: [1 or more characters], actual: %s.", actual)
 	}
 }
 
@@ -18,6 +18,6 @@ func TestAnimalWithParams(t *testing.T) {
 	if err != nil {
 		t.Errorf("AnimalWithParams() execution error: %s", err.Error())
 	} else if len(actual) <= 0 {
-		t.Errorf("AnimalWithParams() was incorrect, expect: [1 or more character], actual: %s.", actual)
+		t.Errorf("AnimalWithParams() was incorrect, expect: [1 or more characters], actual: %s.", actual)
 	}
 }
---
+++ character_test.go
@@ -0,0 +1,23 @@
+package chance
+
+import (
+	"testing"
+)
+
+func TestCharacter(t *testing.T) {
+	c := NewChance()
+	actual := c.Character()
+	if len(actual) != 1 {
+		t.Errorf("Character() was incorrect, expect: [exactly 1 character], actual: %s.", actual)
+	}
+}
+
+func TestCharacterWithParams(t *testing.T) {
+	c := NewChance()
+	actual, err := c.CharacterWithParams(true, true, true, true)
+	if err != nil {
+		t.Errorf("CharacterWithParams() execution error: %s", err.Error())
+	} else if len(actual) != 1 {
+		t.Errorf("Character() was incorrect, expect: [exactly 1 character], actual: %s.", actual)
+	}
+}
