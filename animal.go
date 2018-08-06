094ff81c4757d046e9c347489622a1caabf06ad4
Travis <travis@localhost.dev>
2018-08-06 15:44:42 +1000

    Complete AnimalWithParams()

--- animal.go
+++ animal.go
@@ -1,19 +1,26 @@
 package chance
 
 import (
-	"math/rand"
+	"errors"
 	"reflect"
 
 	"github.com/rockacola/go-chance/data"
 )
 
 func (c *Chance) Animal() string {
-	animalTypes := reflect.ValueOf(data.Animal).MapKeys()
-	animalType := animalTypes[0].Interface().(string) // Golang natively randomize its key orders, hence simply picking the first item is sufficient.
-	randomIndex := rand.Intn(len(data.Animal[animalType]) - 1)
-	return data.Animal[animalType][randomIndex]
+	categories := reflect.ValueOf(data.Animal).MapKeys()
+	category := categories[0].Interface().(string) // Golang natively randomize its key orders, hence simply picking the first item is sufficient.
+	randomIndex := c.Rand.Intn(len(data.Animal[category]) - 1)
+	return data.Animal[category][randomIndex]
 }
 
 func (c *Chance) AnimalWithParams(category string) (string, error) {
-	return "aaaa", nil
+	animalsOfCategory := data.Animal[category]
+	if len(animalsOfCategory) <= 0 {
+		return "", errors.New("Invalid animal category.")
+	}
+
+	randomIndex := c.Rand.Intn(len(animalsOfCategory) - 1)
+	output := animalsOfCategory[randomIndex]
+	return output, nil
 }
