6c4c776bb47bea84210bb94233cbe91dcf806395
Travis <travis@localhost.dev>
2018-08-15 20:10:53 +1000

    Tweak firstName test

--- first_name_test.go
+++ first_name_test.go
@@ -7,19 +7,19 @@ import (
 func TestFirstName(t *testing.T) {
 	c := NewChance()
 	actual := c.FirstName()
-	if len(actual) <= 0 {
+	if len(actual) <= 1 {
 		t.Errorf("FirstName() was incorrect, expect: [1 or more characters], actual: %s.", actual)
 	}
 }
 
-func TestFirstNamelWithParams(t *testing.T) {
+func TestFirstNameWithParams(t *testing.T) {
 	c := NewChance()
 	gender := "male"
 	nationality := "it"
 	actual, err := c.FirstNameWithParams(gender, nationality)
 	if err != nil {
 		t.Errorf("FirstNameWithParams() execution error: %s", err.Error())
-	} else if len(actual) <= 0 {
+	} else if len(actual) <= 1 {
 		t.Errorf("FirstNameWithParams() was incorrect, expect: [1 or more characters], actual: %s.", actual)
 	}
 }
