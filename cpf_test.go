bf2ca02dfdf16a54e20479a81330b3fdf2333e95
Travis <travis@localhost.dev>
2018-08-15 19:56:34 +1000

    Tweak test for Cpf()

--- cpf_test.go
+++ cpf_test.go
@@ -7,7 +7,7 @@ import (
 func TestCpf(t *testing.T) {
 	c := NewChance()
 	actual := c.Cpf()
-	if len(actual) < 9 {
-		t.Errorf("Cpf() was incorrect, expect: [9 or more characters], actual: %s.", actual)
+	if len(actual) <= 7 {
+		t.Errorf("Cpf() was incorrect, expect: [7 or more characters], actual: %s.", actual)
 	}
 }
