5145d13d5dbeb235701100c2e0dfaae693325be6
Travis <travis@localhost.dev>
2018-08-26 17:22:39 +1000

    Comment tweak

--- android_id.go
+++ android_id.go
@@ -1,6 +1,6 @@
 package chance
 
-// Generate an Android GCM Registration identifier.
+// Generate a random Android GCM Registration identifier.
 func (c *Chance) AndroidId() string {
 	return "APA91" + c.stringFromPool(178, "0123456789abcefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_")
 }
--- apple_token.go
+++ apple_token.go
@@ -4,7 +4,7 @@ import (
 	"github.com/rockacola/go-chance/data"
 )
 
-// Generate an Apple Push Token.
+// Generate a random Apple Push Token.
 func (c *Chance) AppleToken() string {
 	return c.stringFromPool(64, data.Characters["hex"])
 }
